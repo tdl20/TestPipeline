059158aedd7b2466e1448580eb79e12b7dd1fd06
tdl20 <dp1.0a1.tandung@gmail.com>
2021-12-11 21:29:27 -0500

    first commit

---
+++ src/main/java/com/example/milestone1b/Milestone1BApplication.java
@@ -0,0 +1,13 @@
+package com.example.milestone1b;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Milestone1BApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Milestone1BApplication.class, args);
+    }
+
+}
