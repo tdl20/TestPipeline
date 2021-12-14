9f85bf8e9f7565543289fd5456251343cf8e0220
tdl20 <95966537+tdl20@users.noreply.github.com>
2021-12-14 12:42:14 +0700

    Update Milestone1BApplication.java

--- src/main/java/com/example/milestone1b/Milestone1BApplication.java
+++ src/main/java/com/example/milestone1b/Milestone1BApplication.java
@@ -1,10 +1,12 @@
 package com.example.milestone1b;
+import javax.servlet.*;
+import javax.servlet.http.*;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Milestone1BApplication {
+public class Milestone1BApplication extends HttpServlet {
 
     public static void main(String[] args) {
         SpringApplication.run(Milestone1BApplication.class, args);
